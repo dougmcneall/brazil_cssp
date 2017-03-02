f8d798fe10b1075dfcf2fd4163011db5b0eddfc8
dougmcneall <dougmcneall@gmail.com>
2017-03-02 10:37:47 +0000

    linking into github files

--- test_write_jules_design.R
+++ test_write_jules_design.R
@@ -2,6 +2,13 @@
 # Tests and examples for write_jules_design.R
 # Doug McNeall dougmcneall@gmail.com
 
+
+# Source design writing function
+source('https://raw.githubusercontent.com/dougmcneall/brazil_cssp/master/write_jules_design.R')
+
+# Load Andy Wiltshire's paramter choices
+source('https://raw.githubusercontent.com/dougmcneall/brazil_cssp/master/jules_params_awiltshire.R')
+
 fac = c('g_root_io', 'retran_l_io')
 minfac = c(0.5, 0.5)
 maxfac = c(2,2)
