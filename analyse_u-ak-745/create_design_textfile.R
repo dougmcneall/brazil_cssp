8c840b3aa9610ed17072eb62f2069b7b0572b48f
dougmcneall <dougmcneall@gmail.com>
2017-10-23 10:29:15 +0100

    Looking for ways to optimise needleleaf in boost_needleleaf.R

--- analyse_u-ak-745/create_design_textfile.R
+++ analyse_u-ak-745/create_design_textfile.R
@@ -1,7 +1,13 @@
 # create_design_textfile.R
 
 setwd("/Users/dougmcneall/Documents/work/R/brazil_cssp/analyse_u-ak-745")
+library(MASS)
 
 lhs <- read.table('lhs_u-ak745.txt', header = TRUE)
 
 rownames(lhs) <- paste0("P", sprintf("%04d", 0:99))
+
+write.table(lhs, file = 'u-ak745_design.txt')
+write.csv(lhs, file = 'u-ak745_design.csv')
+
+
