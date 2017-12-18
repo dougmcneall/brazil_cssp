a61268560a285c68886ba775a02d8702a3dfbd78
dougmcneall <dougmcneall@gmail.com>
2017-12-18 15:00:21 +0000

    Updating to include new screening ensemble u-ao732 and its partners.

--- analyse_u-ak-745/lasso_vs_stepwise.R
+++ analyse_u-ak-745/lasso_vs_stepwise.R
@@ -2,7 +2,6 @@
 # Testing the lasso vs stepwise regression in selecting inputs.
 #
 
-
 setwd("/Users/dougmcneall/Documents/work/R/brazil_cssp/analyse_u-ak-745")
 ## Try and build an emulator
 library(devtools)
@@ -79,24 +78,66 @@ cn <- colnames(lhs)
 X.norm <- normalize(lhs)
 
 x <- X.norm
-y <- c(sam_area_means_vegfrac.list[[2]], recursive = TRUE)
-y <- blet_sam_mae
+y <- sqrt(c(sam_area_means_vegfrac.list[[2]], recursive = TRUE))
+#y <- blet_sam_mae
 
 dat = data.frame(y=y, x=X.norm)
 colnames(dat) = c('y', colnames(X.norm))
 
-# Lasso section
-fit.glmnet.cv = cv.glmnet(x,y)
-coef(fit.glmnet.cv, s = "lambda.1se")
 
-coefs.fit.glmnet.chosen = coef(fit.glmnet.cv, s = "lambda.1se")
+twoStep = function(X, y, nugget=NULL, nuggetEstim=FALSE, noiseVar=NULL, seed=NULL, trace=FALSE, maxit=100,
+                   REPORT=10, factr=1e7, pgtol=0.0, parinit=NULL, popsize=100){
+  
+  control_list = list(trace=trace, maxit=maxit, REPORT=REPORT, factr=factr, pgtol=pgtol, pop.size=popsize)
+  
+  xvars = colnames(X)
+  data = data.frame(response=y, x=X)
+  colnames(data) <- c("response", xvars)
+  nval = length(y)
+  
+  # Build the first emulator with a flat prior
+  m0 = km(y ~ 1, design=X, response=y, nugget=nugget,
+          nugget.estim=nuggetEstim, noise.var=noiseVar, control=control_list)
+  
+  coefs0 = m0@covariance@range.val 
+  print('coefs0')
+  print(coefs0)
+  
+  start.form = as.formula(paste("y ~ ", paste(xvars, collapse= "+")))
+  
+  # use BIC so that model is parsimonious and allow GP to pick up any other behaviour not
+  # explained by the key linear terms      
+  startlm = lm(start.form, data=data)
+  #      print('before step')
+  #      print(startlm)
+  steplm = step(startlm, direction="both", k=log(nval), trace=TRUE)
+  print('after step')
+  print(steplm)
+  form = as.formula(steplm)
+  print('Formula')
+  print(form)
+  data$response = NULL
+  labels = labels(terms(steplm))
+  labels = labels[!(labels %in% c('response'))]
+  if (length(labels) > 0) {
+    start.form = as.formula(paste("~ ", paste(labels, collapse= "+")))
+  } else {
+    start.form = as.formula("~ 1")
+  }    
+  print("Step has found formula:")
+  print(start.form)
+  if (!is.null(seed)) {set.seed(seed)}
+  m = km(start.form, design=X, response=y, nugget=nugget, parinit=coefs0,
+         nugget.estim=nuggetEstim, noise.var=noiseVar, control=control_list)
+  
+  return(list(x=X, y=y, nugget=nugget, nugget.estim=nuggetEstim,
+              noise.var=noiseVar, emulator=m, seed=seed, coefs=m@covariance@range.val,
+              trends=m@trend.coef, meanTerms=all.vars(start.form), steplm = steplm))
+  
+}
 
-plot(coefs.fit.glmnet.chosen)
+#twostep = twoStep(X, y, nugget = 0.001, nuggetEstim = FALSE, maxit = 1000)
 
-nd = matrix(rep(0, 74), nrow = 1)
-colnames(nd) = colnames(X.norm)
-
-predict(fit.glmnet.cv, s = "lambda.1se", newx = nd)
 
 true.loo = function(X,y,type){
   
@@ -141,6 +182,20 @@ true.loo = function(X,y,type){
       #out.sd[i = pred$sd]
     }
     
+    else if(type == 'gpflat'){
+      fit = km(~1, design = X.trunc, response = y.trunc)
+      pred = predict(fit,newdata = X.target, type = 'UK')
+      out.mean[i] = pred$mean
+      #out.sd[i = pred$sd]
+    }
+    
+    else if(type == 'twoStep'){
+      fit = twoStep(X = X.trunc, y = y.trunc, nuggetEstim = TRUE, maxit = 1000)
+      pred = predict(fit$emulator,newdata = X.target, type = 'UK')
+      out.mean[i] = pred$mean
+      #out.sd[i = pred$sd]
+    }
+    
   }
   return(list(mean = out.mean))
 }  
@@ -153,6 +208,8 @@ test.lm = true.loo(X = X.norm, y = y, type = 'lm')
 test.sw = true.loo(X = X.norm, y = y, type = 'sw')
 test.lasso = true.loo(X = X.norm, y = y, type = 'lasso')
 test.gp = true.loo(X = X.norm, y = y, type = 'gp')
+test.gpflat = true.loo(X = X.norm, y = y, type = 'gpflat')
+test.twoStep = true.loo(X = X.norm, y = y, type = 'twoStep')
 # How does cross validation perform compared to stepwise?
 # How are the coefficients rlative to setepwise?
 
@@ -164,25 +221,23 @@ rmse = function(y, ydash){
   out
 }
 
-rmse(y, test.lm$mean )
-rmse(y, test.sw$mean )
-rmse(y, test.lasso$mean )
-rmse(y, test.gp$mean )
+rmse(y^2, test.lm$mean^2 )
+rmse(y^2, test.sw$mean^2 )
+rmse(y^2, test.lasso$mean^2 )
+rmse(y^2, test.gp$mean^2 )
+rmse(y^2, test.gpflat$mean^2 )
+rmse(y^2, test.twoStep$mean^2 )
 
 
-  
-  
 plot(y, test.lm$mean, col = 'black', pch = 19,
-     xlim = c(0.1, 0.5), ylim = c(0.1,0.5))
+     xlim = c(0, 1), ylim = c(0,1))
 
 points(y, test.sw$mean, col = 'red', pch = 19)
 points(y, test.gp$mean, col = 'orange', pch = 19)
 points(y, test.lasso$mean, col = 'blue', pch = 19)
+points(y, test.gpflat$mean, col = 'purple', pch = 19)
+points(y, test.twoStep$mean, col = 'grey', pch = 19)
 abline(0,1)
-
-
-
-
 
 
 # Stepwise section
@@ -195,12 +250,44 @@ start.form = as.formula(paste("~ ", paste(labels, collapse= "+")))
 # Feed both the lasso and the Stepwise fits into a Gaussian
 # process to see how they perform.
 
+# Is "twoStep" (stepwise then GP) better than everything else?
+# Are there any data transformations which might help?
+
+# Lasso section
+fit.glmnet.cv = cv.glmnet(x,y)
+coef(fit.glmnet.cv, s = "lambda.1se")
+
+# The labels of the retained coefficients are here
+# (retains intercept at index zero)
+coef.i = (coef(fit.glmnet.cv, s = "lambda.1se"))@i
+
+labs = labels(coef(fit.glmnet.cv, s = "lambda.1se"))[[1]]
+labs = labs[-1] # remove intercept
+glmnet.retained = labs[coef.i]
+
+start.form = as.formula(paste("~ ", paste(glmnet.retained , collapse= "+")))
 
 # Build the first emulator with a flat prior
-m0 = km(y ~ 1, design=X, response=y, nugget=nugget,
-        nugget.estim=nuggetEstim, noise.var=noiseVar, control=control_list)
+m0 = km(y ~ 1, design=X.norm, response=y, nugget.estim = TRUE)
+
+m0 = km(y ~., design=X.norm, response=y, nugget.estim = TRUE)
 
 coefs0 = m0@covariance@range.val 
+
+m1 = km(start.form, design=X.norm, response=y,
+        parinit=coefs0, nugget.estim = TRUE)
+
+m2 = km(~., design=X.norm, response=y,
+        parinit=coefs0, nugget.estim = TRUE)
+
+
+
+nd = matrix(rep(0.5, 74), nrow = 1)
+colnames(nd) = colnames(X.norm)
+
+predict(fit.glmnet.cv, s = "lambda.1se", newx = nd)
+
+
 
 
 
---
+++ analyze_u-ao732/initial_analysis_u-ao732.R
@@ -0,0 +1,6 @@
+# initial_analysis_u-ao732
+
+source('../per_pft.R')
+
+lhs = read.table('data/lhs_u-ao732.txt', header = TRUE)
+
